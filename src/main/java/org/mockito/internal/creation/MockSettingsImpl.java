12c1e9a5103324e2c585f9fde49edacb4040fba7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:16:14 -0700

    Revert "fix: 2 - DEREFERENCE_NULLABLE - List<Object> resultArgs = new ArrayList<Object>(constructorArgs.length + 1); - dereferenced expression constructorArgs is @Nullable"
    This reverts commit eae56d1a76f870a0629e84eb3e888854e0517878.

--- src/main/java/org/mockito/internal/creation/MockSettingsImpl.java
+++ src/main/java/org/mockito/internal/creation/MockSettingsImpl.java
@@ -156,9 +156,6 @@ public class MockSettingsImpl<T> extends CreationSettings<T>
   @Nullable
   @Override
   public Object[] getConstructorArgs() {
-    if (constructorArgs == null) {
-      return null;
-    }
     if (outerClassInstance == null) {
       return constructorArgs;
     }
