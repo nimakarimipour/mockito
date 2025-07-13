c4845a9e4ea106538153a4857d95c1a36037b53c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:13:11 -0700

    Revert "fix: 12 - PASS_NULLABLE - assureArguments(constructor, null, null, arguments, constructor.getParameterTypes()); - passing @Nullable parameter 'null' where @NonNull is required"
    This reverts commit e7c49367c9777f758edfd32c5915ef22cb0f342f.

--- src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
+++ src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
@@ -128,12 +128,7 @@ class InstrumentationMemberAccessor implements MemberAccessor {
       throw new InstantiationException(
           "Cannot instantiate abstract " + constructor.getDeclaringClass().getTypeName());
     }
-    assureArguments(
-        constructor,
-        constructor.getDeclaringClass(),
-        constructor.getParameterTypes(),
-        arguments,
-        constructor.getParameterTypes());
+    assureArguments(constructor, null, null, arguments, constructor.getParameterTypes());
     try {
       Object module = getModule.bindTo(constructor.getDeclaringClass()).invokeWithArguments();
       String packageName = constructor.getDeclaringClass().getPackage().getName();
