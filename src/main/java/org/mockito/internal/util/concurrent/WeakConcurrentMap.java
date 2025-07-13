389ccb314e7ba33fa6b8558c9ccc77e323ee53b1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:24:01 -0700

    fix: 35 - ASSIGN_FIELD_NULLABLE - this.entry = entry; - assigning @Nullable expression to @NonNull field

--- src/main/java/org/mockito/internal/util/concurrent/WeakConcurrentMap.java
+++ src/main/java/org/mockito/internal/util/concurrent/WeakConcurrentMap.java
@@ -4,6 +4,7 @@
  */
 package org.mockito.internal.util.concurrent;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
@@ -344,7 +345,7 @@ public class WeakConcurrentMap<K, V> extends ReferenceQueue<K>
 
     private final K key;
 
-    final Map.Entry<WeakKey<K>, V> entry;
+    @Nullable final Map.Entry<WeakKey<K>, V> entry;
 
     private SimpleEntry(K key, @Nullable Map.Entry<WeakKey<K>, V> entry) {
       this.key = key;
@@ -358,12 +359,16 @@ public class WeakConcurrentMap<K, V> extends ReferenceQueue<K>
 
     @Override
     public V getValue() {
-      return entry.getValue();
+      if (entry != null) {
+        return Nullability.castToNonnull(entry.getValue(), "Entry is not null");
+      }
+      throw new NullPointerException("Entry is null");
     }
 
     @Override
     public V setValue(V value) {
       if (value == null) throw new NullPointerException();
+      if (entry == null) throw new NullPointerException("Entry is null");
       return entry.setValue(value);
     }
   }
