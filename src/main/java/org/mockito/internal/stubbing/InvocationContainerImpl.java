6b7e79f4611b26535d00af83616c2e9c142df881
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:12:57 -0700

    fix: 10 - METHOD_NO_INIT - public InvocationContainerImpl(MockCreationSettings mockSettings) { - initializer method does not guarantee @NonNull field invocationForStubbing (line 38) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- src/main/java/org/mockito/internal/stubbing/InvocationContainerImpl.java
+++ src/main/java/org/mockito/internal/stubbing/InvocationContainerImpl.java
@@ -39,8 +39,9 @@ public class InvocationContainerImpl implements InvocationContainer, Serializabl
 
   public InvocationContainerImpl(MockCreationSettings mockSettings) {
     this.registeredInvocations = createRegisteredInvocations(mockSettings);
-    this.mockStrictness = mockSettings.isLenient() ? Strictness.LENIENT : null;
+    this.mockStrictness = mockSettings.isLenient() ? Strictness.LENIENT : Strictness.STRICT_STUBS;
     this.doAnswerStyleStubbing = new DoAnswerStyleStubbing();
+    this.invocationForStubbing = new MatchableInvocation(); // Ensure non-null initialization
   }
 
   public void setInvocationForPotentialStubbing(MatchableInvocation invocation) {
