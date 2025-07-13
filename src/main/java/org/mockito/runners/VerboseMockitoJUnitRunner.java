f036b4008733b2c7020a563c8df74aa8d0e934ae
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:14:54 -0700

    fix: 29 - DEREFERENCE_NULLABLE - String warnings = warningsCollector.getWarnings(); - dereferenced expression warningsCollector is @Nullable

--- src/main/java/org/mockito/runners/VerboseMockitoJUnitRunner.java
+++ src/main/java/org/mockito/runners/VerboseMockitoJUnitRunner.java
@@ -54,8 +54,10 @@ public class VerboseMockitoJUnitRunner extends Runner implements Filterable {
           @Override
           @SuppressWarnings("deprecation")
           public void testFailure(final Failure failure) throws Exception {
-            String warnings = warningsCollector.getWarnings();
-            new JUnitFailureHacker().appendWarnings(failure, warnings);
+            if (warningsCollector != null) {
+              String warnings = warningsCollector.getWarnings();
+              new JUnitFailureHacker().appendWarnings(failure, warnings);
+            }
           }
         };
 
