af68c291ca79c15ad2057d1260740bc02d0cf0d4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:12:13 -0700

    Revert "fix: 3 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method org.mockito.internal.verification.api.VerificationDataInOrder.getWanted() returns @NonNull"
    This reverts commit 421b1a0ac328d64541567ab293aadfec96e52776.

--- src/main/java/org/mockito/internal/verification/api/VerificationDataInOrderImpl.java
+++ src/main/java/org/mockito/internal/verification/api/VerificationDataInOrderImpl.java
@@ -34,6 +34,6 @@ public class VerificationDataInOrderImpl implements VerificationDataInOrder {
 
   @Nullable
   public MatchableInvocation getWanted() {
-    return Objects.requireNonNull(wanted, "Wanted invocation cannot be null");
+    return wanted;
   }
 }
