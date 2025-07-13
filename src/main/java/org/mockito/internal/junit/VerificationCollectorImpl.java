500173124ba7a0b67270e4809956388bc2c7293e
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:23:15 -0700

    before:17to16 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression message is @Nullable' /home/nima/Developer/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/junit/VerificationCollectorImpl.java:82         .append(message.trim())

--- src/main/java/org/mockito/internal/junit/VerificationCollectorImpl.java
+++ src/main/java/org/mockito/internal/junit/VerificationCollectorImpl.java
@@ -74,14 +74,16 @@ public class VerificationCollectorImpl implements VerificationCollector {
   }
 
   private void append(@Nullable String message) {
-    this.numberOfFailures++;
-    this.builder
-        .append('\n')
-        .append(this.numberOfFailures)
-        .append(". ")
-        .append(message.trim())
-        .append('\n');
-  }
+      if (message != null) {
+        this.numberOfFailures++;
+        this.builder
+            .append('\n')
+            .append(this.numberOfFailures)
+            .append(". ")
+            .append(message.trim())
+            .append('\n');
+      }
+    }
 
   private class VerificationWrapper implements VerificationMode {
 
