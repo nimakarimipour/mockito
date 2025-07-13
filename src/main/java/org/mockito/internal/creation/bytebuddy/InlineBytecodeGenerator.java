28e743b71a8394f3ba694ffb176917232cc017ab
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:13:54 -0700

    fix: 19 - DEREFERENCE_NULLABLE - if (!modules.contains(module) && !(Boolean) canRead.invoke(module, target)) { - dereferenced expression canRead is @Nullable

--- src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java
@@ -279,8 +279,11 @@ public class InlineBytecodeGenerator implements BytecodeGenerator, ClassFileTran
                   null));
       for (Class<?> type : types) {
         Object module = getModule.invoke(type);
-        if (!modules.contains(module) && !(Boolean) canRead.invoke(module, target)) {
-          modules.add(module);
+        if (!modules.contains(module)) {
+          Boolean canReadResult = (Boolean) canRead.invoke(module, target);
+          if (canReadResult != null && !canReadResult) {
+            modules.add(module);
+          }
         }
       }
       for (Object module : modules) {
