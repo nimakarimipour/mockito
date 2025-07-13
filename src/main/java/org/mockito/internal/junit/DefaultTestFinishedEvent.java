0e20dca6d3f614704e29a41c15b1d34c34caa351
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:13:41 -0700

    fix: 17 - DEREFERENCE_NULLABLE - return testClassInstance.getClass().getSimpleName() + "." + testMethodName; - dereferenced expression testClassInstance is @Nullable

--- src/main/java/org/mockito/internal/junit/DefaultTestFinishedEvent.java
+++ src/main/java/org/mockito/internal/junit/DefaultTestFinishedEvent.java
@@ -26,6 +26,9 @@ public class DefaultTestFinishedEvent implements TestFinishedEvent {
 
   @Override
   public String getTestName() {
+    if (testClassInstance == null) {
+      throw new NullPointerException("testClassInstance is null");
+    }
     return testClassInstance.getClass().getSimpleName() + "." + testMethodName;
   }
 }
