0531f9a39e4806c5fa55ebd2301ebdca406e610c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:15:33 -0700

    Revert "fix: 35 - ASSIGN_FIELD_NULLABLE - this.entry = entry; - assigning @Nullable expression to @NonNull field"
    This reverts commit 4f8e773120f4d13f662834e34520f69c54321f86.

--- src/main/java/org/mockito/internal/util/concurrent/WeakConcurrentMap.java
+++ src/main/java/org/mockito/internal/util/concurrent/WeakConcurrentMap.java
@@ -347,7 +347,6 @@ public class WeakConcurrentMap<K, V> extends ReferenceQueue<K>
     final Map.Entry<WeakKey<K>, V> entry;
 
     private SimpleEntry(K key, @Nullable Map.Entry<WeakKey<K>, V> entry) {
-      if (entry == null) throw new NullPointerException("entry is marked @NonNull but is null");
       this.key = key;
       this.entry = entry;
     }
