293058ddf202d60bc0cc04f9b39d4dffcfc74c88
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:22:03 -0700

    fix: 27 - PASS_NULLABLE - assureArguments( - passing @Nullable parameter 'Modifier.isStatic(method.getModifiers()) ? null : target' where @NonNull is required

--- src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
+++ src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
@@ -7,6 +7,7 @@ package org.mockito.internal.util.reflection;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static org.mockito.internal.util.StringUtil.join;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.lang.instrument.Instrumentation;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -157,7 +158,7 @@ class InstrumentationMemberAccessor implements MemberAccessor {
       throws InvocationTargetException {
     assureArguments(
         method,
-        Modifier.isStatic(method.getModifiers()) ? null : target,
+        Nullability.castToNonnull(Modifier.isStatic(method.getModifiers()) ? null : target),
         method.getDeclaringClass(),
         arguments,
         method.getParameterTypes());
