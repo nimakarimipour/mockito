4d6a04fb150cc0382c5caacdc1685bba03aafb6b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:21:30 -0700

    before:30to29 - Type='PASS_NULLABLE', message='passing @Nullable parameter 'cause' where @NonNull is required' /home/nima/Developer/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/creation/bytebuddy/MockMethodAdvice.java:330               hideRecursiveCall(

--- src/main/java/org/mockito/internal/creation/bytebuddy/MockMethodAdvice.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/MockMethodAdvice.java
@@ -319,19 +319,21 @@ public class MockMethodAdvice extends MockMethodDispatcher {
   }
 
   private static Object tryInvoke(Method origin, @Nullable Object instance, Object[] arguments)
-      throws Throwable {
-    MemberAccessor accessor = Plugins.getMemberAccessor();
-    try {
-      return accessor.invoke(origin, instance, arguments);
-    } catch (InvocationTargetException exception) {
-      Throwable cause = exception.getCause();
-      new ConditionalStackTraceFilter()
-          .filter(
-              hideRecursiveCall(
-                  cause, new Throwable().getStackTrace().length, origin.getDeclaringClass()));
-      throw cause;
+        throws Throwable {
+      MemberAccessor accessor = Plugins.getMemberAccessor();
+      try {
+        return accessor.invoke(origin, instance, arguments);
+      } catch (InvocationTargetException exception) {
+        Throwable cause = exception.getCause();
+        if (cause != null) {
+          new ConditionalStackTraceFilter()
+              .filter(
+                  hideRecursiveCall(
+                      cause, new Throwable().getStackTrace().length, origin.getDeclaringClass()));
+        }
+        throw cause;
+      }
     }
-  }
 
   private static class ReturnValueWrapper implements Callable<Object> {
 
