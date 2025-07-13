f2ab2df204869d5959ab836cdd23ae2830bc82be
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:13:30 -0700

    Revert "fix: 15 - PASS_NULLABLE - assureArguments( - passing @Nullable parameter 'Modifier.isStatic(field.getModifiers()) ? null : target' where @NonNull is required"
    This reverts commit f56bc459fe81af03e42fab2cefeda2b46b7948ea.

--- src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
+++ src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
@@ -193,9 +193,6 @@ class InstrumentationMemberAccessor implements MemberAccessor {
 
   @Override
   public Object get(Field field, Object target) {
-    if (!Modifier.isStatic(field.getModifiers()) && target == null) {
-      throw new IllegalArgumentException("Target cannot be null for non-static fields");
-    }
     assureArguments(
         field,
         Modifier.isStatic(field.getModifiers()) ? null : target,
