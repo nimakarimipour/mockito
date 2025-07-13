de27f91e182cbbaa5ee16a2bfdb24421f9b6e0b7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:14:07 -0700

    fix: 21 - RETURN_NULLABLE - return fieldInstance != null ? fieldInstance.getClass() : null; - returning @Nullable expression from method with @NonNull return type

--- src/main/java/org/mockito/internal/util/reflection/FieldInitializationReport.java
+++ src/main/java/org/mockito/internal/util/reflection/FieldInitializationReport.java
@@ -51,6 +51,6 @@ public class FieldInitializationReport {
    * @return Class of the instance
    */
   public Class<?> fieldClass() {
-    return fieldInstance != null ? fieldInstance.getClass() : null;
+    return fieldInstance != null ? fieldInstance.getClass() : Object.class;
   }
 }
