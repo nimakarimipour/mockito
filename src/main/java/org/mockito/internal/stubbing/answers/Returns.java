96421c859e8671e62b6b9231b161c184c9e71f20
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:20:35 -0700

    before:35to34 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression value is @Nullable' /home/nima/Developer/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/stubbing/answers/Returns.java:51     return value.getClass().getSimpleName();

--- src/main/java/org/mockito/internal/stubbing/answers/Returns.java
+++ src/main/java/org/mockito/internal/stubbing/answers/Returns.java
@@ -48,8 +48,11 @@ public class Returns implements Answer<Object>, ValidableAnswer, Serializable {
   }
 
   private String printReturnType() {
-    return value.getClass().getSimpleName();
-  }
+        if (value == null) {
+            return "null";
+        }
+        return value.getClass().getSimpleName();
+    }
 
   private Class<?> returnType() {
     return value.getClass();
