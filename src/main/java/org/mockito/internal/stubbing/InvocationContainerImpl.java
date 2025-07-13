def4015206eb51e07306ad663ac33ff89a37bcb1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:12:52 -0700

    Revert "fix: 9 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit ad54710ec59ae2424151baf336bb222a7ec7557d.

--- src/main/java/org/mockito/internal/stubbing/InvocationContainerImpl.java
+++ src/main/java/org/mockito/internal/stubbing/InvocationContainerImpl.java
@@ -93,13 +93,15 @@ public class InvocationContainerImpl implements InvocationContainer, Serializabl
       for (StubbedInvocationMatcher s : stubbed) {
         if (s.matches(invocation)) {
           s.markStubUsed(invocation);
+          // TODO we should mark stubbed at the point of stubbing, not at the point where
+          // the stub is being used
           invocation.markStubbed(new StubInfoImpl(s));
           return s;
         }
       }
     }
 
-    throw new IllegalStateException("No matching stubbed invocation found");
+    return null;
   }
 
   /** Sets the answers declared with 'doAnswer' style. */
