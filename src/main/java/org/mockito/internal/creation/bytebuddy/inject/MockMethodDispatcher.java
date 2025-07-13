9e9e2749d2b7643456b74c5cebf498488db45ba9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:15:19 -0700

    fix: 33 - DEREFERENCE_NULLABLE - return DISPATCHERS - dereferenced expression DISPATCHERS         .get(identifier) is @Nullable

--- src/main/java/org/mockito/internal/creation/bytebuddy/inject/MockMethodDispatcher.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/inject/MockMethodDispatcher.java
@@ -45,16 +45,17 @@ public abstract class MockMethodDispatcher {
   }
 
   @Nullable
-  @SuppressWarnings("unused")
   public static Object handleConstruction(
       String identifier,
       Class<?> type,
       Object object,
       Object[] arguments,
       String[] parameterTypeNames) {
-    return DISPATCHERS
-        .get(identifier)
-        .handleConstruction(type, object, arguments, parameterTypeNames);
+    MockMethodDispatcher dispatcher = DISPATCHERS.get(identifier);
+    if (dispatcher == null) {
+      throw new IllegalArgumentException("No dispatcher found for identifier: " + identifier);
+    }
+    return dispatcher.handleConstruction(type, object, arguments, parameterTypeNames);
   }
 
   @Nullable
