176c6be37f6f1b7ad58a8cb4f9f6ade7fcaf0c3f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 09:23:44 -0700

    before:15to14 - Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/nima/Developer/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/util/concurrent/WeakConcurrentMap.java:351       this.entry = entry;

--- src/main/java/org/mockito/internal/util/concurrent/WeakConcurrentMap.java
+++ src/main/java/org/mockito/internal/util/concurrent/WeakConcurrentMap.java
@@ -347,9 +347,10 @@ public class WeakConcurrentMap<K, V> extends ReferenceQueue<K>
     final Map.Entry<WeakKey<K>, V> entry;
 
     private SimpleEntry(K key, @Nullable Map.Entry<WeakKey<K>, V> entry) {
-      this.key = key;
-      this.entry = entry;
-    }
+          if (entry == null) throw new NullPointerException("entry is marked @NonNull but is null");
+          this.key = key;
+          this.entry = entry;
+      }
 
     @Override
     public K getKey() {
