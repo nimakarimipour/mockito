fc557c575767c4e94305ee1c42e9b80a65bce6a8
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:16:06 -0700

    fix: 1 - ASSIGN_FIELD_NULLABLE - this.wanted = wanted; - assigning @Nullable expression to @NonNull field

--- src/main/java/org/mockito/internal/verification/VerificationDataImpl.java
+++ src/main/java/org/mockito/internal/verification/VerificationDataImpl.java
@@ -17,7 +17,7 @@ import org.mockito.invocation.MatchableInvocation;
 
 public class VerificationDataImpl implements VerificationData {
 
-  private final InvocationMatcher wanted;
+  @Nullable private final InvocationMatcher wanted;
   private final InvocationContainerImpl invocations;
 
   public VerificationDataImpl(
@@ -32,11 +32,13 @@ public class VerificationDataImpl implements VerificationData {
     return invocations.getInvocations();
   }
 
+  @Nullable
   @Override
   public MatchableInvocation getTarget() {
     return wanted;
   }
 
+  @Nullable
   @Override
   public InvocationMatcher getWanted() {
     return wanted;
