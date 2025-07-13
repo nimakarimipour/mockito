6c13a5bf4b64bf1a8c86baecac7d71bff1022236
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:14:21 -0700

    Revert "fix: 23 - DEREFERENCE_NULLABLE - logger.log(warningsCollector.getWarnings()); - dereferenced expression warningsCollector is @Nullable"
    This reverts commit 245098ca4da61dae580691db9997dc08a14a3f85.

--- src/main/java/org/mockito/runners/ConsoleSpammingMockitoJUnitRunner.java
+++ src/main/java/org/mockito/runners/ConsoleSpammingMockitoJUnitRunner.java
@@ -53,9 +53,7 @@ public class ConsoleSpammingMockitoJUnitRunner extends Runner implements Filtera
 
           @Override
           public void testFailure(Failure failure) throws Exception {
-            if (warningsCollector != null) {
-              logger.log(warningsCollector.getWarnings());
-            }
+            logger.log(warningsCollector.getWarnings());
           }
         };
 
