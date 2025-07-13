506b95ef6ab4663d40bc4557e7ff9a0d6881fe49
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:21:39 -0700

    fix: 23 - DEREFERENCE_NULLABLE - logger.log(warningsCollector.getWarnings()); - dereferenced expression warningsCollector is @Nullable

--- src/main/java/org/mockito/runners/ConsoleSpammingMockitoJUnitRunner.java
+++ src/main/java/org/mockito/runners/ConsoleSpammingMockitoJUnitRunner.java
@@ -53,7 +53,9 @@ public class ConsoleSpammingMockitoJUnitRunner extends Runner implements Filtera
 
           @Override
           public void testFailure(Failure failure) throws Exception {
-            logger.log(warningsCollector.getWarnings());
+            if (warningsCollector != null) {
+              logger.log(warningsCollector.getWarnings());
+            }
           }
         };
 
