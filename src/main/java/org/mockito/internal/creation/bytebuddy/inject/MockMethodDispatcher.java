2ff35d83113ce269653f329d664ead1e90799692
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:14:32 -0700

    fix: 25 - DEREFERENCE_NULLABLE - return DISPATCHERS.get(identifier).isConstructorMock(type); - dereferenced expression DISPATCHERS.get(identifier) is @Nullable

--- src/main/java/org/mockito/internal/creation/bytebuddy/inject/MockMethodDispatcher.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/inject/MockMethodDispatcher.java
@@ -39,9 +39,9 @@ public abstract class MockMethodDispatcher {
     DISPATCHERS.putIfAbsent(identifier, dispatcher);
   }
 
-  @SuppressWarnings("unused")
   public static boolean isConstructorMock(String identifier, Class<?> type) {
-    return DISPATCHERS.get(identifier).isConstructorMock(type);
+    MockMethodDispatcher dispatcher = DISPATCHERS.get(identifier);
+    return dispatcher != null && dispatcher.isConstructorMock(type);
   }
 
   @Nullable
